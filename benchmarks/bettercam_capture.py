f12117189fd1881133a29133368cf003dcb6620e
Elijah Harmon <elijahharmon@gmail.com>
2023-11-16 20:52:03 -0800

    updated benchmarks

--- benchmarks/bettercam_capture.py
+++ benchmarks/bettercam_capture.py
@@ -10,8 +10,8 @@ region = (LEFT, TOP, RIGHT, BOTTOM)
 title = "[BetterCam] Capture benchmark"
 
 fps = 0
-camera = bettercam.create(output_idx=0)
-camera.start(target_fps=60)
+camera = bettercam.create(output_idx=0, output_color="BGRA")
+camera.start(target_fps=60, video_mode=True)
 for i in range(1000):
     image = camera.get_latest_frame()
 camera.stop()
--- benchmarks/dxcam_capture.py
+++ benchmarks/dxcam_capture.py
@@ -10,8 +10,8 @@ region = (LEFT, TOP, RIGHT, BOTTOM)
 title = "[dxcam] Capture benchmark"
 
 fps = 0
-camera = dxcam.create(output_idx=0)
-camera.start(target_fps=60)
+camera = dxcam.create(output_idx=0, output_color="BGRA")
+camera.start(target_fps=60, video_mode=True)
 for i in range(1000):
     image = camera.get_latest_frame()
 camera.stop()
